4db95678dc9958704e6090514a2b7c5d05789868
hg786km <hagu0987@outlook.com>
2023-08-04 13:34:43 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { ArrowRightIcon } from "@heroicons/react/solid";
+
+export default function Navbar() {
+  return (
+    <header className=" text-gray-400 bg-gray-800 md:sticky top-0 z-10">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <div className="title-font font-medium text-white mb-4 md:mb-0">
+              <a href="#About" className="ml-3 text-xl">
+                Read Barger
+              </a>
+          </div>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a href = "#Projects" className="mr-5 hover:text-white">
+              Past Work
+            </a>
+            <a href = "#Skills" className="mr-5 hover:text-white">
+              Skills
+            </a>
+            <a href = "#Testimonials" className="mr-5 hover:text-white">
+              Testimonials
+            </a>
+            <a href = "#Contact" className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+              Hire Me
+              <ArrowRightIcon className="w-4 h-4 ml-1" />
+            </a>
+          </nav>
+        </div>
+    </header>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+    "C++","Java","Python","Django","Angular","React",
+  ];
+
+  export const testimonials = [
+    {
+        quote:"Hi, Harsh is great",
+        image:"./testimonials-1.gif",
+        name:"Guy1",
+        company:"C1",
+    },
+    {
+        quote:"Hi, Harsh is smart",
+        image:"./testimonials-2.gif",
+        name:"Guy2",
+        company:"C2",
+    },
+    {
+        quote:"Hardworking guy",
+        image:"./testimonials-3.gif",
+        name:"Guy3",
+        company:"C3",
+    },
+    {
+        quote:"Nice to have him as a colleague",
+        image:"./testimonials-4.gif",
+        name:"Guy4",
+        company:"C4",
+    },
+  ];
